ee4a65abb5c194de18e1394c2af58b5669668011
Isabelle Villasenor <villasenori@outlook.com>
2024-01-15 15:00:00 +0100

    Add ExTest

--- shuffle_array/shuffle_array.exs
+++ shuffle_array/shuffle_array.exs
@@ -10,3 +10,19 @@ defmodule Solution do
     %{state | shuffled: shuffled_list}
   end
 end
+
+defmodule SolutionTest do
+  use ExUnit.Case
+  alias Solution
+
+  test "shuffles and resets an array" do
+    nums = [1, 2, 3]
+    solution = Solution.new(nums)
+
+    shuffled = Solution.shuffle(solution)
+    assert MapSet.new(shuffled.shuffled) == MapSet.new(nums)
+
+    reset = Solution.reset(shuffled)
+    assert reset.shuffled == nums
+  end
+end
