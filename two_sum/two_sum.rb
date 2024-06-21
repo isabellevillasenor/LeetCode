3d778cbfbba327cef5478f77518a14a9b61333f5
Isabelle Villasenor <villasenori@outlook.com>
2024-06-21 14:00:00 +0100

    Remove comments

--- two_sum/two_sum.rb
+++ two_sum/two_sum.rb
@@ -1,15 +1,9 @@
 require 'rspec'
 require 'pry'
-# @param {Integer[]} nums
-# @param {Integer} target
-# @return {Integer[]}
+
 class Test
   def two_sum(nums, target)
-  # iterate over [nums] and add until == target
-  # return nums placement in [] i.e. 0, 1
-  binding.pry
     nums.each_with_index do |num, index|
-        binding.pry
     end
   end
 end
