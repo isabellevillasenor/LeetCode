1087f846d567b7264b83a816d4e4177273eac7fc
Isabelle Villasenor <villasenori@outlook.com>
2024-06-21 17:40:00 +0100

    Add test

--- two_sum/two_sum.go
+++ two_sum/two_sum.go
@@ -18,3 +18,30 @@ func twoSum(nums []int, target int) []int {
 func main() {
     fmt.Println(twoSum([]int{2, 7, 11, 15}, 9)) // Output: [0, 1]
 }
+
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	tests := []struct {
+		nums   []int
+		target int
+		want   []int
+	}{
+		{[]int{2, 7, 11, 15}, 9, []int{0, 1}},
+		{[]int{3, 2, 4}, 6, []int{1, 2}},
+		{[]int{-3, 4, 3, 90}, 0, []int{0, 2}},
+		{[]int{0, 4, 3, 0}, 0, []int{0, 3}},
+	}
+
+	for _, tt := range tests {
+		got := twoSum(tt.nums, tt.target)
+		if !reflect.DeepEqual(got, tt.want) {
+			t.Errorf("twoSum(%v, %d) = %v; want %v", tt.nums, tt.target, got, tt.want)
+		}
+	}
+}
