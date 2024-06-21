fe21ab3e7eedecae23f6e3bb73d84bdbe6ba1af0
Isabelle Villasenor <villasenori@outlook.com>
2024-06-21 16:00:00 +0100

    Add Spec

--- two_sum/two_sum.rb
+++ two_sum/two_sum.rb
@@ -15,9 +15,25 @@ class Test
   
 end
 
-describe 'test' do
-  it 'returns needed' do
+
+describe 'Test#two_sum' do
+  it 'returns the correct indices for a valid case' do
     test = Test.new
-    test.two_sum([2,7,11,15], 9)
+    expect(test.two_sum([2, 7, 11, 15], 9)).to eq([0, 1])
+  end
+
+  it 'returns the correct indices when numbers repeat' do
+    test = Test.new
+    expect(test.two_sum([3, 2, 4], 6)).to eq([1, 2])
+  end
+
+  it 'returns the correct indices for negative numbers' do
+    test = Test.new
+    expect(test.two_sum([-3, 4, 3, 90], 0)).to eq([0, 2])
+  end
+
+  it 'returns the correct indices when target is zero' do
+    test = Test.new
+    expect(test.two_sum([0, 4, 3, 0], 0)).to eq([0, 3])
   end
 end
