2fbf514f3f22893ce2250c3147d92a6eea8b1fc3
Isabelle Villasenor <villasenori@outlook.com>
2024-06-24 11:00:00 +0100

    Add Elixir Spec

--- two_sum/two_sum.exs
+++ two_sum/two_sum.exs
@@ -15,3 +15,24 @@ end
 
 # Example usage
 IO.inspect(TwoSum.two_sum([2, 7, 11, 15], 9)) # Output: [0, 1]
+
+defmodule TwoSumTest do
+  use ExUnit.Case
+  doctest TwoSum
+
+  test "finds correct indices for basic case" do
+    assert TwoSum.two_sum([2, 7, 11, 15], 9) == [0, 1]
+  end
+
+  test "finds correct indices when numbers repeat" do
+    assert TwoSum.two_sum([3, 2, 4], 6) == [1, 2]
+  end
+
+  test "handles negative numbers" do
+    assert TwoSum.two_sum([-3, 4, 3, 90], 0) == [0, 2]
+  end
+
+  test "handles zero sum case" do
+    assert TwoSum.two_sum([0, 4, 3, 0], 0) == [0, 3]
+  end
+end
