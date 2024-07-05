5a15de409ef6d1decf28e3719e3de6dfd64c2ef9
Isabelle Villasenor <villasenori@outlook.com>
2024-07-05 15:00:00 +0100

    Add Spec

--- validate_binary_search/validate_binary_search.rb
+++ validate_binary_search/validate_binary_search.rb
@@ -1,3 +1,5 @@
+require 'rspec'
+
 class TreeNode
   attr_accessor :val, :left, :right
 
@@ -18,10 +20,27 @@ class Solution
   def valid_bst?(node, min, max)
     return true if node.nil?
 
-    # Ensure the current node's value is within the valid range
     return false if node.val <= min || node.val >= max
 
-    # Recursively check the left and right subtrees
     valid_bst?(node.left, min, node.val) && valid_bst?(node.right, node.val, max)
+  end
+end
+
+RSpec.describe Solution do
+  let(:solution) { Solution.new }
+
+  describe '#is_valid_bst' do
+    it 'returns true for a valid BST' do
+      # Creating a valid BST: 
+      #       2
+      #      / \
+      #     1   3
+      root = TreeNode.new(2)
+      root.left = TreeNode.new(1)
+      root.right = TreeNode.new(3)
+
+      expect(solution.is_valid_bst(root)).to be true
+    end
+
   end
 end
