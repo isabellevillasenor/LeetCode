d0e220b430d861d2668b8f9a9f93144fc5f899d4
Isabelle Villasenor <villasenori@outlook.com>
2024-07-05 14:00:00 +0100

    Add Solution Class

--- validate_binary_search/validate_binary_search.rb
+++ validate_binary_search/validate_binary_search.rb
@@ -8,4 +8,20 @@ class TreeNode
   end
 end
 
+class Solution
+  def is_valid_bst(root)
+    valid_bst?(root, -Float::INFINITY, Float::INFINITY)
+  end
 
+  private
+
+  def valid_bst?(node, min, max)
+    return true if node.nil?
+
+    # Ensure the current node's value is within the valid range
+    return false if node.val <= min || node.val >= max
+
+    # Recursively check the left and right subtrees
+    valid_bst?(node.left, min, node.val) && valid_bst?(node.right, node.val, max)
+  end
+end
