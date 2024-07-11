a415601e036485281dd9c49ee60ba95a856ec870
Isabelle Villasenor <villasenori@outlook.com>
2024-07-11 13:00:00 +0100

    Add Secondary Go Function

--- validate_binary_search/validate_binary_search.go
+++ validate_binary_search/validate_binary_search.go
@@ -12,3 +12,14 @@ func isValidBST(root *TreeNode) bool {
     return validateBST(root, math.MinInt64, math.MaxInt64)
 }
 
+func validateBST(node *TreeNode, min, max int) bool {
+	if node == nil {
+			return true
+	}
+
+	if node.Val <= min || node.Val >= max {
+			return false
+	}
+
+	return validateBST(node.Left, min, node.Val) && validateBST(node.Right, node.Val, max)
+}
