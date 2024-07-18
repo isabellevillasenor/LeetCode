effbb8509376f6f976bc71791d881eae5c3529d8
Isabelle Villasenor <villasenori@outlook.com>
2024-07-18 13:00:00 +0100

    Add Ruby Class

---
+++ first_bad_version/first_bad_version.rb
@@ -0,0 +1,21 @@
+# The API isBadVersion is already defined for you.
+# @param {Integer} version
+# @return {boolean} whether the version is bad or not
+# def isBadVersion(version)
+
+def first_bad_version(n)
+  left = 1
+  right = n
+
+  while left < right
+    mid = left + (right - left) / 2
+
+    if is_bad_version(mid)
+      right = mid # The bad version is on the left side or is mid itself
+    else
+      left = mid + 1 # The bad version is on the right side
+    end
+  end
+
+  left
+end
