9b93f40a58a76283994d384b822778510b62013b
Isabelle Villasenor <villasenori@outlook.com>
2024-08-27 13:30:00 +0100

    Add push Method

--- min_stack/min_stack.rb
+++ min_stack/min_stack.rb
@@ -4,4 +4,10 @@ class MinStack
     @min_stack = []
   end
 
+  def push(val)
+    @stack.push(val)
+    @min_stack.push([val, @min_stack.empty? ? val : [val, @min_stack.last[1]].min])
+  end
+
+
 end
