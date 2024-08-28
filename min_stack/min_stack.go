c9abbf9a5dde110c80813cd6fd79e14f7057a29a
Isabelle Villasenor <villasenori@outlook.com>
2024-08-28 11:00:00 +0100

    Add Func

--- min_stack/min_stack.go
+++ min_stack/min_stack.go
@@ -9,4 +9,9 @@ func Constructor() MinStack {
 	return MinStack{}
 }
 
-
+func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
+	if len(this.minStack) == 0 || val <= this.GetMin() {
+		this.minStack = append(this.minStack, val)
+	}
+}
