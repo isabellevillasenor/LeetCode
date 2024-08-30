f9dd8949c58df61470c2b38a35a84ef400f6a45c
Isabelle Villasenor <villasenori@outlook.com>
2024-08-30 13:00:00 +0100

    Add Rspec

--- count_primes/count_primes.rb
+++ count_primes/count_primes.rb
@@ -14,3 +14,18 @@ class Solution
     is_prime.count(true)
   end
 end
+
+require 'rspec'
+
+RSpec.describe Solution do
+  let(:solution) { Solution.new }
+
+  it 'counts primes correctly' do
+    expect(solution.count_primes(10)).to eq(4)  # Primes: 2, 3, 5, 7
+    expect(solution.count_primes(0)).to eq(0)
+    expect(solution.count_primes(1)).to eq(0)
+    expect(solution.count_primes(2)).to eq(0)
+    expect(solution.count_primes(3)).to eq(1)   # Prime: 2
+    expect(solution.count_primes(100)).to eq(25)
+  end
+end
