6a25c819005c2e5748e10a4aa6dcce8ac31efc78
Isabelle Villasenor <villasenori@outlook.com>
2024-09-06 13:00:00 +0100

    Add Elixir Method

---
+++ valid_parentheses/valid_parentheses.exs
@@ -0,0 +1,25 @@
+defmodule Solution do
+  @pairs %{")" => "(", "}" => "{", "]" => "["}
+
+  def is_valid(s) do
+    s
+    |> String.graphemes()
+    |> check_valid([])
+  end
+
+  defp check_valid([], []), do: true
+  defp check_valid([], _), do: false
+
+  defp check_valid([char | rest], stack) do
+    cond do
+      @pairs[char] && stack != [] && hd(stack) == @pairs[char] ->
+        check_valid(rest, tl(stack))  # Pop from stack
+      
+      @pairs[char] ->
+        false  # Invalid if stack is empty and closing bracket found
+
+      true ->
+        check_valid(rest, [char | stack])  # Push onto stack
+    end
+  end
+end
