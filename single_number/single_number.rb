e9fb0bb22410e1189416f3a9c213f46932761e0b
Isabelle Villasenor <villasenori@outlook.com>
2024-10-07 13:00:00 +0100

    Add Rspec

--- single_number/single_number.rb
+++ single_number/single_number.rb
@@ -3,3 +3,13 @@ class Solution
     nums.reduce(:^) # XOR approach
   end
 end
+
+
+RSpec.describe Solution do
+  it 'finds the single number' do
+    solution = Solution.new
+    expect(solution.single_number([2,2,1])).to eq(1)
+    expect(solution.single_number([4,1,2,1,2])).to eq(4)
+    expect(solution.single_number([1])).to eq(1)
+  end
+end
