5c007a3bbe0784266e82a4ea04c07cb7a2b6edb5
Isabelle Villasenor <villasenori@outlook.com>
2024-10-08 15:00:00 +0100

    Add Test

--- single_number/single_file.go
+++ single_number/single_file.go
@@ -7,3 +7,25 @@ func singleNumber(nums []int) int {
     }
     return result
 }
+
+package main
+
+import "testing"
+
+func TestSingleNumber(t *testing.T) {
+    tests := []struct {
+        input  []int
+        output int
+    }{
+        {[]int{2, 2, 1}, 1},
+        {[]int{4, 1, 2, 1, 2}, 4},
+        {[]int{1}, 1},
+    }
+
+    for _, test := range tests {
+        result := singleNumber(test.input)
+        if result != test.output {
+            t.Errorf("Expected %d but got %d", test.output, result)
+        }
+    }
+}
