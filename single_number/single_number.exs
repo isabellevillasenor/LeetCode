95c3bf70cc983363872209c5c5d6b8db8764e925
Isabelle Villasenor <villasenori@outlook.com>
2024-10-09 12:00:00 +0100

    Add Elixir Test

--- single_number/single_number.exs
+++ single_number/single_number.exs
@@ -3,3 +3,14 @@ defmodule Solution do
     Enum.reduce(nums, 0, &Bitwise.bxor/2) # XOR approach
   end
 end
+
+defmodule SolutionTest do
+  use ExUnit.Case
+  doctest Solution
+
+  test "finds the single number" do
+    assert Solution.single_number([2, 2, 1]) == 1
+    assert Solution.single_number([4, 1, 2, 1, 2]) == 4
+    assert Solution.single_number([1]) == 1
+  end
+end
