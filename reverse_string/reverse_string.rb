5e7e30466e494aa4d262e804c15908b8cd4efb5e
Isabelle Villasenor <villasenori@outlook.com>
2024-10-11 14:00:00 +0100

    Add Solution Rspec

--- reverse_string/reverse_string.rb
+++ reverse_string/reverse_string.rb
@@ -3,3 +3,19 @@ class Solution
     s.reverse!
   end
 end
+
+require 'rspec'
+
+RSpec.describe Solution do
+  it 'reverses the string' do
+    solution = Solution.new
+    arr1 = ['h', 'e', 'l', 'l', 'o']
+    arr2 = ['H', 'a', 'n', 'n', 'a', 'h']
+
+    solution.reverse_string(arr1)
+    solution.reverse_string(arr2)
+
+    expect(arr1).to eq(['o', 'l', 'l', 'e', 'h'])
+    expect(arr2).to eq(['h', 'a', 'n', 'n', 'a', 'H'])
+  end
+end
