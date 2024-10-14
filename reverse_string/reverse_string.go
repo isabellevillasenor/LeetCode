196bb88715bf6a90099484dfa092288abddb524e
Isabelle Villasenor <villasenori@outlook.com>
2024-10-14 14:00:00 +0100

    Add Go Test

--- reverse_string/reverse_string.go
+++ reverse_string/reverse_string.go
@@ -8,3 +8,27 @@ func reverseString(s []byte) {
         right--
     }
 }
+
+package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestReverseString(t *testing.T) {
+	tests := []struct {
+		input  []byte
+		output []byte
+	}{
+		{[]byte{'h', 'e', 'l', 'l', 'o'}, []byte{'o', 'l', 'l', 'e', 'h'}},
+		{[]byte{'H', 'a', 'n', 'n', 'a', 'h'}, []byte{'h', 'a', 'n', 'n', 'a', 'H'}},
+	}
+
+	for _, test := range tests {
+		reverseString(test.input)
+		if !reflect.DeepEqual(test.input, test.output) {
+			t.Errorf("Expected %v but got %v", test.output, test.input)
+		}
+	}
+}
