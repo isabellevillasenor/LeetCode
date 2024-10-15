61f91092d5681b46897794e42e4174034599b1c3
Isabelle Villasenor <villasenori@outlook.com>
2024-10-15 12:00:00 +0100

    Add Test

--- reverse_string/reverse_string.exs
+++ reverse_string/reverse_string.exs
@@ -1,3 +1,13 @@
 defmodule Solution do
   def reverse_string(s), do: Enum.reverse(s)
 end
+
+defmodule SolutionTest do
+  use ExUnit.Case
+  doctest Solution
+
+  test "reverses the string" do
+    assert Solution.reverse_string(['h', 'e', 'l', 'l', 'o']) == ['o', 'l', 'l', 'e', 'h']
+    assert Solution.reverse_string(['H', 'a', 'n', 'n', 'a', 'h']) == ['h', 'a', 'n', 'n', 'a', 'H']
+  end
+end
