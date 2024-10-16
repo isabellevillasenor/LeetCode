1a3d417f1bec264926ad7b61b9dcfdb6207f9fb1
Isabelle Villasenor <villasenori@outlook.com>
2024-10-16 13:30:00 +0100

    Add Soltuion Class with has_cycle Method

--- linked_list_cycle/linked_list_cycle.rb
+++ linked_list_cycle/linked_list_cycle.rb
@@ -6,4 +6,17 @@ class ListNode
   end
 end
 
+class Solution
+  def has_cycle(head)
+    slow = head
+    fast = head
 
+    while fast && fast.next
+      slow = slow.next
+      fast = fast.next.next
+      return true if slow == fast
+    end
+
+    false
+  end
+end
