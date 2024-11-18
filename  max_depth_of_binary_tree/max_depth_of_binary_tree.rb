9bc42a8d43e33f4c91ad3fe6a5cd36345df0d989
Isabelle Villasenor <villasenori@outlook.com>
2024-11-18 12:00:00 +0100

    Add test 1

---  max_depth_of_binary_tree/max_depth_of_binary_tree.rb
+++  max_depth_of_binary_tree/max_depth_of_binary_tree.rb
@@ -13,3 +13,26 @@ class Solution
     1 + [max_depth(root.left), max_depth(root.right)].max
   end
 end
+
+require 'rspec'
+
+RSpec.describe Solution do
+  let(:solution) { Solution.new }
+
+  it 'calculates max depth correctly' do
+    root = TreeNode.new(3)
+    root.left = TreeNode.new(9)
+    root.right = TreeNode.new(20, TreeNode.new(15), TreeNode.new(7))
+
+    expect(solution.max_depth(root)).to eq(3)
+  end
+
+  # it 'returns 0 for an empty tree' do
+  #   expect(solution.max_depth(nil)).to eq(0)
+  # end
+
+  # it 'returns 1 for a single node' do
+  #   root = TreeNode.new(1)
+  #   expect(solution.max_depth(root)).to eq(1)
+  # end
+end
