aef694724b662d3feed0a2180b5ade8893ce98a3
Isabelle Villasenor <villasenori@outlook.com>
2024-11-18 09:40:00 +0100

    Add Init for TreeNode Class

---  max_depth_of_binary_tree/max_depth_of_binary_tree.rb
+++  max_depth_of_binary_tree/max_depth_of_binary_tree.rb
@@ -1,10 +1,10 @@
 class TreeNode
-  # attr_accessor :val, :left, :right
-  # def initialize(val = 0, left = nil, right = nil)
-  #   @val = val
-  #   @left = left
-  #   @right = right
-  # end
+  attr_accessor :val, :left, :right
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
 end
 
 class Solution
