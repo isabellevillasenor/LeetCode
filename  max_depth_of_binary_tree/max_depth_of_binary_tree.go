c9773f83a49ec924293a3bd1254e605daec1d87d
Isabelle Villasenor <villasenori@outlook.com>
2024-11-19 17:00:00 +0100

    Add Go Tests

---  max_depth_of_binary_tree/max_depth_of_binary_tree.go
+++  max_depth_of_binary_tree/max_depth_of_binary_tree.go
@@ -21,3 +21,26 @@ func max(a, b int) int {
     }
     return b
 }
+
+package main
+
+import "testing"
+
+func TestMaxDepth(t *testing.T) {
+    root := &TreeNode{Val: 3}
+    root.Left = &TreeNode{Val: 9}
+    root.Right = &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}}
+
+    if maxDepth(root) != 3 {
+        t.Errorf("Expected 3, got %d", maxDepth(root))
+    }
+
+    if maxDepth(nil) != 0 {
+        t.Errorf("Expected 0, got %d", maxDepth(nil))
+    }
+
+    singleNode := &TreeNode{Val: 1}
+    if maxDepth(singleNode) != 1 {
+        t.Errorf("Expected 1, got %d", maxDepth(singleNode))
+    }
+}
