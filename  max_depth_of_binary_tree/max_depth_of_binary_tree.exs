a4ec231dfeec590c23ced8ed8c8e1aaa02d54f9d
Isabelle Villasenor <villasenori@outlook.com>
2024-11-20 15:00:00 +0100

    Add Elixir Test

---  max_depth_of_binary_tree/max_depth_of_binary_tree.exs
+++  max_depth_of_binary_tree/max_depth_of_binary_tree.exs
@@ -4,3 +4,26 @@ defmodule Solution do
     1 + max(max_depth(left), max_depth(right))
   end
 end
+
+defmodule SolutionTest do
+  use ExUnit.Case
+
+  defmodule TreeNode do
+    defstruct val: 0, left: nil, right: nil
+  end
+
+  test "calculates max depth correctly" do
+    root = %TreeNode{val: 3, left: %TreeNode{val: 9}, right: %TreeNode{val: 20, left: %TreeNode{val: 15}, right: %TreeNode{val: 7)}}
+
+    assert Solution.max_depth(root) == 3
+  end
+
+  test "returns 0 for an empty tree" do
+    assert Solution.max_depth(nil) == 0
+  end
+
+  test "returns 1 for a single node" do
+    root = %TreeNode{val: 1}
+    assert Solution.max_depth(root) == 1
+  end
+end
