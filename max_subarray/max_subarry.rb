46bdf32476a41b0cefa320383235a7ae012ff0f1
Isabelle Villasenor <villasenori@outlook.com>
2024-11-22 14:00:00 +0100

    Add Rspec

--- max_subarray/max_subarry.rb
+++ max_subarray/max_subarry.rb
@@ -11,3 +11,21 @@ class Solution
     max_sum
   end
 end
+
+require 'rspec'
+
+RSpec.describe Solution do
+  let(:solution) { Solution.new }
+
+  it 'returns the maximum subarray sum' do
+    expect(solution.max_sub_array([-2,1,-3,4,-1,2,1,-5,4])).to eq(6)
+  end
+
+  it 'returns the only element if array has one element' do
+    expect(solution.max_sub_array([1])).to eq(1)
+  end
+
+  it 'handles all negative numbers' do
+    expect(solution.max_sub_array([-1,-2,-3,-4])).to eq(-1)
+  end
+end
