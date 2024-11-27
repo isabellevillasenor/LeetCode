5cecf59f16cf6a863dbe374b6efc1f5ef4ee75fa
Isabelle Villasenor <villasenori@outlook.com>
2024-11-27 13:00:00 +0100

    Add climb_stairs Method

--- climbing_stairs/climbing_stairs.rb
+++ climbing_stairs/climbing_stairs.rb
@@ -1,13 +1,13 @@
 class Solution
-  # def climb_stairs(n)
-  #   return n if n <= 2
+  def climb_stairs(n)
+    return n if n <= 2
 
-  #   prev1, prev2 = 1, 2
+    prev1, prev2 = 1, 2
 
-  #   (3..n).each do
-  #     prev1, prev2 = prev2, prev1 + prev2
-  #   end
+    (3..n).each do
+      prev1, prev2 = prev2, prev1 + prev2
+    end
 
-  #   prev2
-  # end
+    prev2
+  end
 end
