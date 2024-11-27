d2c06f9ea42ee44bcb315454ff795cb5177959fb
Isabelle Villasenor <villasenori@outlook.com>
2024-11-27 15:00:00 +0100

    Add Rspec

--- climbing_stairs/climbing_stairs.rb
+++ climbing_stairs/climbing_stairs.rb
@@ -11,3 +11,15 @@ class Solution
     prev2
   end
 end
+
+require 'rspec'
+
+RSpec.describe Solution do
+  let(:solution) { Solution.new }
+
+  it 'returns the number of ways to climb stairs' do
+    expect(solution.climb_stairs(2)).to eq(2)
+    expect(solution.climb_stairs(3)).to eq(3)
+    expect(solution.climb_stairs(5)).to eq(8)
+  end
+end
