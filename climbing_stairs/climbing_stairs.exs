55fc0d3ebc106377563c80602f6605c08cfa37f2
Isabelle Villasenor <villasenori@outlook.com>
2024-11-29 13:00:00 +0100

    Add ExTest

--- climbing_stairs/climbing_stairs.exs
+++ climbing_stairs/climbing_stairs.exs
@@ -6,3 +6,14 @@ defmodule Solution do
   defp climb(2, _prev1, prev2), do: prev2
   defp climb(n, prev1, prev2), do: climb(n - 1, prev2, prev1 + prev2)
 end
+
+defmodule SolutionTest do
+  use ExUnit.Case
+  doctest Solution
+
+  test "returns the number of ways to climb stairs" do
+    assert Solution.climb_stairs(2) == 2
+    assert Solution.climb_stairs(3) == 3
+    assert Solution.climb_stairs(5) == 8
+  end
+end
