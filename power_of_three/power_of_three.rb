cc52ce0d7efd1940500cac0f60553b45f6641321
Isabelle Villasenor <villasenori@outlook.com>
2025-01-17 12:00:00 +0100

    Add Rspec

--- power_of_three/power_of_three.rb
+++ power_of_three/power_of_three.rb
@@ -4,3 +4,17 @@ class Solution
     1162261467 % n == 0
   end
 end
+
+require 'rspec'
+
+RSpec.describe Solution do
+  it 'checks if a number is a power of three' do
+    solution = Solution.new
+
+    expect(solution.is_power_of_three(27)).to be true
+    expect(solution.is_power_of_three(9)).to be true
+    expect(solution.is_power_of_three(1)).to be true
+    expect(solution.is_power_of_three(0)).to be false
+    expect(solution.is_power_of_three(45)).to be false
+  end
+end
