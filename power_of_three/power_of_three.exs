0d1b21386cf85967839f43f47b72689f395f9e53
Isabelle Villasenor <villasenori@outlook.com>
2025-01-22 12:00:00 +0100

    Add ExTest

--- power_of_three/power_of_three.exs
+++ power_of_three/power_of_three.exs
@@ -2,3 +2,16 @@ defmodule Solution do
   def is_power_of_three(n) when n > 0, do: rem(1162261467, n) == 0
   def is_power_of_three(_), do: false
 end
+
+defmodule SolutionTest do
+  use ExUnit.Case
+  alias Solution
+
+  test "checks if a number is a power of three" do
+    assert Solution.is_power_of_three(27) == true
+    assert Solution.is_power_of_three(9) == true
+    assert Solution.is_power_of_three(1) == true
+    assert Solution.is_power_of_three(0) == false
+    assert Solution.is_power_of_three(45) == false
+  end
+end
